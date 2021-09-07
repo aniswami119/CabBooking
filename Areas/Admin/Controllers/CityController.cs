82b8e24601c0d2ffc998a1f5ec40e2fcffabee72
aniswami119 <90129839+aniswami119@users.noreply.github.com>
2021-09-07 22:11:35 +0530

    Add project files.

---
+++ Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,82 @@
+﻿using Book_Cab_App.DataAccess.Repository;
+using Book_Cab_App.DataAccess.Repository.IRepository;
+using Book_Cab_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Cab_App.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+
+    
+     private readonly IUnitOfWork _unitOfWork;
+
+
+    public CityController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Upsert(int? id)
+    {
+        City city = new City();
+        if (id == null)
+            //this is for create a new city
+            return View(city);
+        city = _unitOfWork.City.Get(id.GetValueOrDefault());
+        if (city == null)
+            return NotFound();
+        return View(city);
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Upsert(City city)
+    {
+        if (ModelState.IsValid)
+        {
+            if (city.Id == 0)
+                _unitOfWork.City.Add(city);
+            else
+                _unitOfWork.City.Update(city);
+            _unitOfWork.Save();
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    #region API Calls
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var listOfCities = _unitOfWork.City.GetAll();
+        return Json(new { data = listOfCities });
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int id)
+    {
+        var cityFromDb = _unitOfWork.City.Get(id);
+        if (cityFromDb == null)
+            return Json(new { success = false, message = "Error while deleting the record" });
+        _unitOfWork.City.Remove(cityFromDb);
+        _unitOfWork.Save();
+        return Json(new { success = true, message = "Record Deleted Successfully" });
+    }
+
+    #endregion
+
+}
+}
---
+++ wwwroot/js/city.js
@@ -0,0 +1,60 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable()
+{
+    dataTable= $('#tblCity').DataTable({
+        "ajax": {
+            "url": "/Admin/City/GetAll"
+        },
+        "columns": [
+            {
+                "data":"name","width":"60%"
+            },
+            {
+                "data": "id",
+                "render": function (data)
+                {
+                    return `
+                     <div class="text-center">
+                    <a href="/Admin/City/Upsert/${data}" class="btn btn-success"><i class="fas fa-edit"></i></a>
+                      <a onclick=Delete("/Admin/City/Delete/${data}") class="btn btn-danger text-white"><i class="fas fa-trash-alt"></i></a>
+                    </div>
+                      `;
+                },"width":"40%"
+            }
+        ]
+    })
+}
+
+function Delete(url)
+{
+    swal({
+        title: "Are you sure, do you want to delete this record?",
+        text: "You will not be able to restore the data later once deleted",
+        icon: "warning",
+        buttons: true,
+        dangerModel:true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data)
+                {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                                       
+                    else
+                        toastr.error(data.message);
+                }
+            })
+        }
+        
+    })
+}
